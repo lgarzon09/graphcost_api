670d45342f22e1a9d7f39c06de5e50c4ceadf2cf
Andy164 <57103424+Andy164@users.noreply.github.com>
2020-12-12 17:58:38 -0500

    Add functions and add property to model

--- db/bookings_db.py
+++ db/bookings_db.py
@@ -4,8 +4,8 @@ from datetime import date, timedelta
 class BookingsInDB(BaseModel):
   boo_id: int = 0
   boo_cli_id: int
-  boo_dateIN: str
-  boo_dateOUT: str
+  boo_dateIN: date
+  boo_dateOUT: date
   boo_roo_id: int
   boo_name_roo: str
   boo_rec_id: int
@@ -44,26 +44,57 @@ def save_bookings(booking_in_db: BookingsInDB):
   
   return booking_in_db
 
-# def get_bookings_active():
-#   bookings_active = []
-#   today = date.today() - timedelta(days = 1)
-  
-#   for booking in database_bookings:
-#     if date >= today:
-#       bookings_active.append(booking)
-  
-#   return bookings_active
-
-def get_bookings_active(inDate: str, outDate: str):
+def get_bookings_active(inDate: date, outDate: date):
   bookings_active = []
-  inDate = date.fromisoformat(inDate)
-  outDate = date.fromisoformat(outDate)
+
   while inDate <= outDate:
+    bookings_today = []
+    
     for booking in database_bookings:
-      booIn = date.fromisoformat(booking["boo_dateIN"])
-      booOut = date.fromisoformat(booking["boo_dateOUT"])
-      if booIn <= inDate and booOut >= inDate:
-        bookings_active.append(booking)
-    inDate = inDate + timedelta(days = 1)
+      
+      if date.fromisoformat(booking["boo_dateIN"]) <= inDate and date.fromisoformat(booking["boo_dateOUT"]) >= outDate:
+        bookings_today.append(booking)
+    
+    bookings_active.append(bookings_today)
+    inDate += timedelta(days = 1)
 
   return bookings_active
+
+def get_table(price: int, maintenance_cost: int):
+  dic_prices = {0: "", 10: "", 20: "", 30: "", 40: "", 50: "", 60: "", 70: "", 80: "", 90:""}
+
+  profit_margin = (price - maintenance_cost) / price
+
+  if profit_margin < 0:
+    profit_margin = 0
+  
+  multiple = 1 - profit_margin
+  
+  for i in dic_prices:
+    dic_prices[i] = multiple
+    multiple += 0.1
+  
+  return dic_prices
+
+def get_tens(value: int):
+  if value >= 90:
+    value = 90
+  elif value >= 80:
+    value = 80
+  elif value >= 70:
+    value = 70
+  elif value >= 60:
+    value = 60
+  elif value >= 50:
+        value = 50
+  elif value >= 40:
+    value = 40
+  elif value >= 30:
+    value = 30  
+  elif value >= 20:
+        value = 20
+  elif value >= 10:
+    value = 10
+  else:
+    value = 0
+  return value
--- models/bookings_model.py
+++ models/bookings_model.py
@@ -8,13 +8,12 @@ class BookingsIn(BaseModel):
   boo_roo_id: int # Esto es el tipo
   boo_name_roo: str
   boo_rec_id: int # ID del recepcionista
+  boo_price_charged: int
 
 class BookingsOut(BaseModel):
   boo_id: int
   boo_cli_id: int
   boo_dateIN: date
   boo_dateOUT: date
-  boo_roo_id: int
   boo_name_roo: str
-  boo_rec_id: int
   boo_price_charged: int
