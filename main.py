cbc48947e587bd2754d6a2ed52ed92cb92068e57
Andy164 <57103424+Andy164@users.noreply.github.com>
2020-12-12 18:00:46 -0500

    Add HTTP methods, fix some bugs and reorder

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-from db.bookings_db import BookingsInDB, save_bookings, get_bookings_active
+from db.bookings_db import BookingsInDB, save_bookings, get_bookings_active, get_table, get_tens
 from models.bookings_model import BookingsIn, BookingsOut
-from db.rooms_db import RoomsInDB, save_room, get_room, get_all_rooms
+from db.rooms_db import RoomsInDB, save_room, get_room, get_all_rooms, get_total_rooms
 from models.rooms_model import RoomsIn, RoomsOut
-from db.hotel_db import HotelInDB, save_hotel, get_info_hotel, get_total_rooms
+from db.hotel_db import HotelInDB, save_hotel, get_info_hotel
 from models.hotel_model import HotelIn, HotelOut
 from db.clients_db import ClientsInDB, get_client, save_client
 from models.clients_model import ClientsIn, ClientsOut
@@ -14,16 +14,57 @@ from fastapi import HTTPException
 
 api = FastAPI()
 
+@api.post("/bookings/price")
+async def booking_price(booking_in: BookingsIn):
+  #necesito una fecha de entrada y salida de una reserva nueva y tipo de habitacion elegida
+  #generar tabla de multiplos
+  #generar ocupacion
+  #consultar el multiplo correspondiente segun la ocupacion
+  #multiplicar el multiplo con el precio base
+  # precio
+  total_rooms = get_total_rooms()
+  occupied_rooms_list = get_bookings_active(booking_in.boo_dateIN, booking_in.boo_dateOUT)
+
+  occupancy_percentage_list = []
+  for occupied_rooms in occupied_rooms_list:
+    occupancy_percentage = (100 * len(occupied_rooms)) / total_rooms
+    occupancy_percentage_list.append(occupancy_percentage)
+
+  room_in_db = get_room(booking_in.boo_name_roo[0])
+  if room_in_db == None:
+      raise HTTPException(status_code = 404, detail = "El tipo de habitación no existe")
+  
+  multiplier_table = get_table(room_in_db.roo_price, room_in_db.roo_maintenance_cost)
+  
+  sale_price = 0
+  for value in occupancy_percentage_list:
+    ten = get_tens(value)
+    multi = multiplier_table.get(ten)
+    sale_price += multi * room_in_db.roo_price
+  
+  return {"boo_price_charged": sale_price}
+
 # Guardar reservación, este enlace se usa cuando el recepcionista hace click 
 # en hacer reserva después de llenar el formulario
 @api.post("/bookings/new/")
 async def make_reservation(booking_in: BookingsIn):
-  #Falta lógica de precios
-  booking_in_db = BookingsInDB(**booking_in.dict(),  boo_price_charged = 0)
+  booking_in_db = BookingsInDB(**booking_in.dict())
   booking_in_db = save_bookings(booking_in_db)
   booking_out = BookingsOut(**booking_in_db.dict())
 
   return booking_out
+
+# Obtener todos los rooms y su info
+# Este se usará cuando el adm hace click en la opción de habitaciones y aparece el 
+# listado de tipo de habitaciones
+@api.get("/rooms/")
+async def get_rooms():
+  all_rooms = get_all_rooms()
+  all_rooms_out = []
+  for room in all_rooms.values():
+    room_out = RoomsOut(**room.dict())
+    all_rooms_out.append(room_out)
+  return all_rooms_out
 
 # Obtener los datos de un tipo de habitación
 # Este se usará cuando el adm hace click en la opción de habitaciones que hay 
@@ -37,18 +78,6 @@ async def get_room_type(roo_type: str):
   
   room_out = RoomsOut(**room_in_db.dict())
   return room_out
-
-# Obtener todos los rooms y su info
-# Este se usará cuando el adm hace click en la opción de habitaciones y aparece el 
-# listado de tipo de habitaciones
-@api.get("/rooms/")
-async def get_rooms():
-  all_rooms = get_all_rooms()
-  all_rooms_out = []
-  for room in all_rooms.values():
-    room_out = RoomsOut(**room.dict())
-    all_rooms_out.append(room_out)
-  return all_rooms_out
 
 # Guardar un tipo nuevo de room, este lo hace el adm en la 
 # opción de agregar otros tipos de habitación
@@ -77,35 +106,31 @@ async def get_hotel():
   hotel_out = HotelOut(**hotel_in_db.dict())
   return hotel_out
 
-# Obtener gerencia, este es el más complicado, es donde necesito tu lógica Leo
+# Obtener gerencia
 # Por ahora lo puse como get pero también necesitamos el post
-@api.get("/gerencia/")
-async def get_gerencia():
-  total_rooms = get_total_rooms()
-  occupied_rooms = get_bookings_active()
+# @api.get("/gerencia/")
+# async def get_gerencia():
+#   total_rooms = get_total_rooms()
+#   occupied_rooms = get_bookings_active()
 
-  occupancy_percentage = (100 * occupied_rooms) / total_rooms
+#   occupancy_percentage = (100 * occupied_rooms) / total_rooms
 
-
-# Creo que me falta el get y post para reportes pero eso se hace después de 
-# salir de este sprint jejejeje
+# Crear el cliente, lo hace el recepcionista
+@api.post("/clients/new")
+async def make_client(client_in: ClientsIn):
+  client_in_db = ClientsInDB(**client_in.dict())
+  client_in_db = save_client(client_in_db)
+  client_out = ClientsOut(**client_in_db.dict())
+  return client_out
 
 # Obtener el cliente, lo hace el recepcionista
 @api.get("/clients/{cli_docNumber}")
-async def get_client_doc():
+async def get_client_doc(cli_docNumber: str):
   client_in_db = get_client(cli_docNumber)
 
   if client_in_db == None:
     raise HTTPException(status_code = 404, detail = "El cliente no existe")
   
-  client_out = ClientsOut(**client_in_db.dict())
-  return client_out
-
-# Crear el cliente, lo hace el recepcionista
-@api.post("/clients/new")
-async def make_client():
-  client_in_db = ClientsInDB(**client_in.dict())
-  client_in_db = save_client(client_in_db)
   client_out = ClientsOut(**client_in_db.dict())
   return client_out
 
@@ -120,5 +145,5 @@ async def auth_user(user_in: UsersIn):
     if user_in_db.use_password != user_in.use_password:
         return {"Autenticado": False} # Revisar !!!!
 
-    user_out = UsersOut(**user_in.dict())
+    user_out = UsersOut(**user_in_db.dict())
     return user_out
