a6a10e29d137779d03a3f9d096e017352cc0ddcf
Andy164 <57103424+Andy164@users.noreply.github.com>
2020-12-12 12:25:41 -0500

    Fix out

--- models/administrators_model.py
+++ models/administrators_model.py
@@ -1,15 +1,13 @@
 from pydantic import BaseModel
 
 class AdministratorsIn(BaseModel):
-  adm_hot_id: int # Tener en cuenta
+  adm_hot_id: int
   adm_name: str
   adm_doc: str
-  adm_email: str
   adm_phone: str
 
-class AdministratorsIn(BaseModel):
-  adm_hot_id: int # Tener en cuenta
+class AdministratorsOut(BaseModel):
+  adm_hot_id: int
   adm_name: str
   adm_doc: str
-  adm_email: str
   adm_phone: str
