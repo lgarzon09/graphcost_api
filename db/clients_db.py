46cf672c25e120e284748b95ac70921ab82fe3af
Andy164 <57103424+Andy164@users.noreply.github.com>
2020-12-11 20:22:45 -0500

    Add functions

--- db/clients_db.py
+++ db/clients_db.py
@@ -2,7 +2,7 @@ from typing import Dict
 from pydantic import BaseModel
 
 class ClientsInDB(BaseModel):
-  cli_id: int
+  cli_id: int = 0
   cli_name: str
   cli_docNumber: str  
 
@@ -25,3 +25,19 @@ database_clients = {
     "cli_docNumber":"1357924680"
   })
 }
+
+generator = {"id": 3}
+
+def save_client(client_in_db: ClientsInDB):
+  generator["id"] = generator["id"] + 1
+  client_in_db.cli_id = generator["id"]
+
+  database_clients[client_in_db.cli_docNumber] = client_in_db
+
+  return client_in_db
+
+def get_client(client_docNumber: str):
+  if client_docNumber in database_clients.keys():
+    return database_clients[client_docNumber]
+  else:
+    return None
