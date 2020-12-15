3a9ed140fa340f012644b137c9244a10fb671264
Andy164 <57103424+Andy164@users.noreply.github.com>
2020-12-15 07:14:32 -0500

    Add CORS

--- main.py
+++ main.py
@@ -16,6 +16,23 @@ from fastapi import HTTPException
 
 api = FastAPI()
 
+from fastapi.middleware.cors import CORSMiddleware
+
+origins = [
+  "http://localhost.tiangolo.com", 
+  "https://localhost.tiangolo.com",
+  "http://localhost", 
+  "http://localhost:8080",
+]
+
+api.add_middleware(
+  CORSMiddleware, 
+  allow_origins=origins,
+  allow_credentials=True, 
+  allow_methods=["*"], 
+  allow_headers=["*"],
+)
+
 @api.post("/bookings/price/")
 async def booking_price(booking_in: BookingsIn):
   #necesito una fecha de entrada y salida de una reserva nueva y tipo de habitacion elegida
